95e2a7d695c39719775a1404d565eb7fbf73df8f
jax <jax_builder@gamil.com>
2024-01-20 15:58:20 +0800

    add train_script

---
+++ train_script.py
@@ -0,0 +1,184 @@
+
+from subprocess import Popen
+from config import python_exec,infer_device,is_half,exp_root,webui_port_main,webui_port_infer_tts,webui_port_uvr5,webui_port_subfix
+import os
+import traceback
+import torch
+
+
+def train_prepare(inp_text,inp_wav_dir,exp_name,gpu_numbers1a,gpu_numbers1Ba,gpu_numbers1c,bert_pretrained_dir,ssl_pretrained_dir,pretrained_s2G_path):
+    open1abcGenerator = open1abc(inp_text,inp_wav_dir,exp_name,gpu_numbers1a,gpu_numbers1Ba,gpu_numbers1c,bert_pretrained_dir,ssl_pretrained_dir,pretrained_s2G_path)
+    for value in open1abcGenerator:
+        print(value)
+
+ps1abc=[]
+def open1abc(inp_text,inp_wav_dir,exp_name,gpu_numbers1a,gpu_numbers1Ba,gpu_numbers1c,bert_pretrained_dir,ssl_pretrained_dir,pretrained_s2G_path):
+    global ps1abc
+    if (ps1abc == []):
+        opt_dir="%s/%s"%(exp_root,exp_name)
+        try:
+            #############################1a
+            path_text="%s/2-name2text.txt" % opt_dir
+            if(os.path.exists(path_text)==False):
+                config={
+                    "inp_text":inp_text,
+                    "inp_wav_dir":inp_wav_dir,
+                    "exp_name":exp_name,
+                    "opt_dir":opt_dir,
+                    "bert_pretrained_dir":bert_pretrained_dir,
+                    "is_half": str(is_half)
+                }
+                gpu_names=gpu_numbers1a.split("-")
+                all_parts=len(gpu_names)
+                for i_part in range(all_parts):
+                    config.update(
+                        {
+                            "i_part": str(i_part),
+                            "all_parts": str(all_parts),
+                            "_CUDA_VISIBLE_DEVICES": gpu_names[i_part],
+                        }
+                    )
+                    os.environ.update(config)
+                    cmd = '"%s" GPT_SoVITS/prepare_datasets/1-get-text.py'%python_exec
+                    print(cmd)
+                    p = Popen(cmd, shell=True)
+                    ps1abc.append(p)
+                yield "进度：1a-ing", {"__type__": "update", "visible": False}, {"__type__": "update", "visible": True}
+                for p in ps1abc:p.wait()
+
+                opt = []
+                for i_part in range(all_parts):#txt_path="%s/2-name2text-%s.txt"%(opt_dir,i_part)
+                    txt_path = "%s/2-name2text-%s.txt" % (opt_dir, i_part)
+                    with open(txt_path, "r",encoding="utf8") as f:
+                        opt += f.read().strip("\n").split("\n")
+                    os.remove(txt_path)
+                with open(path_text, "w",encoding="utf8") as f:
+                    f.write("\n".join(opt) + "\n")
+
+            yield "进度：1a-done", {"__type__": "update", "visible": False}, {"__type__": "update", "visible": True}
+            ps1abc=[]
+            #############################1b
+            config={
+                "inp_text":inp_text,
+                "inp_wav_dir":inp_wav_dir,
+                "exp_name":exp_name,
+                "opt_dir":opt_dir,
+                "cnhubert_base_dir":ssl_pretrained_dir,
+            }
+            gpu_names=gpu_numbers1Ba.split("-")
+            all_parts=len(gpu_names)
+            for i_part in range(all_parts):
+                config.update(
+                    {
+                        "i_part": str(i_part),
+                        "all_parts": str(all_parts),
+                        "_CUDA_VISIBLE_DEVICES": gpu_names[i_part],
+                    }
+                )
+                os.environ.update(config)
+                cmd = '"%s" GPT_SoVITS/prepare_datasets/2-get-hubert-wav32k.py'%python_exec
+                print(cmd)
+                p = Popen(cmd, shell=True)
+                ps1abc.append(p)
+            yield "进度：1a-done, 1b-ing", {"__type__": "update", "visible": False}, {"__type__": "update", "visible": True}
+            for p in ps1abc:p.wait()
+            yield "进度：1a1b-done", {"__type__": "update", "visible": False}, {"__type__": "update", "visible": True}
+            ps1abc=[]
+            #############################1c
+            path_semantic = "%s/6-name2semantic.tsv" % opt_dir
+            if(os.path.exists(path_semantic)==False):
+                config={
+                    "inp_text":inp_text,
+                    "exp_name":exp_name,
+                    "opt_dir":opt_dir,
+                    "pretrained_s2G":pretrained_s2G_path,
+                    "s2config_path":"GPT_SoVITS/configs/s2.json",
+                }
+                gpu_names=gpu_numbers1c.split("-")
+                all_parts=len(gpu_names)
+                for i_part in range(all_parts):
+                    config.update(
+                        {
+                            "i_part": str(i_part),
+                            "all_parts": str(all_parts),
+                            "_CUDA_VISIBLE_DEVICES": gpu_names[i_part],
+                        }
+                    )
+                    os.environ.update(config)
+                    cmd = '"%s" GPT_SoVITS/prepare_datasets/3-get-semantic.py'%python_exec
+                    print(cmd)
+                    p = Popen(cmd, shell=True)
+                    ps1abc.append(p)
+                yield "进度：1a1b-done, 1cing", {"__type__": "update", "visible": False}, {"__type__": "update", "visible": True}
+                for p in ps1abc:p.wait()
+
+                opt = ["item_name	semantic_audio"]
+                for i_part in range(all_parts):
+                    semantic_path = "%s/6-name2semantic-%s.tsv" % (opt_dir, i_part)
+                    with open(semantic_path, "r",encoding="utf8") as f:
+                        opt += f.read().strip("\n").split("\n")
+                    os.remove(semantic_path)
+                with open(path_semantic, "w",encoding="utf8") as f:
+                    f.write("\n".join(opt) + "\n")
+                yield "进度：all-done", {"__type__": "update", "visible": False}, {"__type__": "update", "visible": True}
+            ps1abc = []
+            yield "一键三连进程结束", {"__type__": "update", "visible": True}, {"__type__": "update", "visible": False}
+        except:
+            traceback.print_exc()
+            close1abc()
+            yield "一键三连中途报错", {"__type__": "update", "visible": True}, {"__type__": "update", "visible": False}
+    else:
+        yield "已有正在进行的一键三连任务，需先终止才能开启下一次任务", {"__type__": "update", "visible": False}, {"__type__": "update", "visible": True}
+
+
+def close1abc():
+    global ps1abc
+    if (ps1abc != []):
+        for p1abc in ps1abc:
+            try:
+                kill_process(p1abc.pid)
+            except:
+                traceback.print_exc()
+        ps1abc=[]
+    return "已终止所有一键三连进程", {"__type__": "update", "visible": True}, {"__type__": "update", "visible": False}
+
+def kill_process(pid):
+    cmd = "kill -9 %s"%pid
+    print(cmd)
+    os.system(cmd)###linux上杀了webui，可能还会没杀干净。。。
+
+
+
+
+ngpu = torch.cuda.device_count()
+gpu_infos = []
+mem = []
+if_gpu_ok = False
+
+if torch.cuda.is_available() or ngpu != 0:
+    for i in range(ngpu):
+        gpu_name = torch.cuda.get_device_name(i)
+        if any(value in gpu_name.upper()for value in ["10","16","20","30","40","A2","A3","A4","P4","A50","500","A60","70","80","90","M4","T4","TITAN","L"]):
+            # A10#A100#V100#A40#P40#M40#K80#A4500
+            if_gpu_ok = True  # 至少有一张能用的N卡
+            gpu_infos.append("%s\t%s" % (i, gpu_name))
+            mem.append(int(torch.cuda.get_device_properties(i).total_memory/ 1024/ 1024/ 1024+ 0.4))
+if if_gpu_ok and len(gpu_infos) > 0:
+    gpu_info = "\n".join(gpu_infos)
+    default_batch_size = min(mem) // 2
+else:
+    gpu_info = "很遗憾您这没有能用的显卡来支持您训练"
+    default_batch_size = 1
+gpus = "-".join([i[0] for i in gpu_infos])
+
+inp_text = "/content/data/list/a.list"
+inp_wav_dir = "/content/data/list/"
+exp_name = "jax_clone_voice"
+gpu_numbers="%s-%s"%(gpus,gpus)
+
+bert_pretrained_dir = "GPT_SoVITS/pretrained_models/chinese-roberta-wwm-ext-large"
+ssl_pretrained_dir = "GPT_SoVITS/pretrained_models/chinese-hubert-base"
+pretrained_s2G_path = "GPT_SoVITS/pretrained_models/s2G488k.pth"
+
+train_prepare(inp_text=inp_text, inp_wav_dir=inp_wav_dir, exp_name=exp_name, gpu_numbers1a=gpu_numbers, gpu_numbers1Ba=gpu_numbers, gpu_numbers1c=gpu_numbers,
+              bert_pretrained_dir=bert_pretrained_dir, ssl_pretrained_dir=ssl_pretrained_dir, pretrained_s2G_path=pretrained_s2G_path )
