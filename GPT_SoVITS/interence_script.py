9afa230e854fc0e5a5690308372768e44715fd53
jax <jax_builder@gamil.com>
2024-02-18 17:29:17 +0800

    add interence script support interence backend

--- GPT_SoVITS/interence_script.py
+++ GPT_SoVITS/interence_script.py
@@ -1,13 +1,6 @@
-from interence_base import get_tts_wav
+from interence_base import get_tts_wav, change_sovits_weights, change_gpt_weights
 import wave
-import os
 import numpy as np
-
-SoVITS_weight_root="SoVITS_weights"
-GPT_weight_root="GPT_weights"
-exp_name = "jax_clone_voice"
-gpt_path = GPT_weight_root + "/" + exp_name + "-e15.ckpt"
-sovits_path = SoVITS_weight_root + "/" + exp_name + "_e8_s120.pth"
 
 def create_wav_file(file_path, audio_data, sampling_rate):
     # 打开WAV文件
@@ -17,37 +10,12 @@ def create_wav_file(file_path, audio_data, sampling_rate):
         wav_file.setsampwidth(2)  # 16位
         wav_file.setframerate(sampling_rate)
         wav_file.setnframes(len(audio_data))
-
         # 将音频数据写入WAV文件
         wav_file.writeframes(np.array(audio_data, dtype=np.int16).tobytes())
 
-def interence(srt_content, audio_list_dir):
-    subs = pysrt.from_string(srt_content)
-    for i, sub in enumerate(subs):
-        wav_name = f"{i+1}.wav"
-        ref_wav_path = audio_list_dir + "/" + wav_name
-        chinese_text = sub.text.split('\n')[1]
-        english_text = sub.text.split('\n')[0]
-        prompt_text = chinese_text
-        prompt_language = "中文"
-        text = english_text
-        text_language = "英文"
-        getTTSWavGenerator = get_tts_wav(ref_wav_path, prompt_text, prompt_language, text, text_language)
-        for simple_rate, audio_data in getTTSWavGenerator:
-            wav_output_dir = "data/output_list"
-            file_path = wav_output_dir + "/" + wav_name
-            os.makedirs(wav_output_dir, exist_ok=True)
-            create_wav_file(file_path=file_path, audio_data=audio_data, sampling_rate=simple_rate)
-
-
-current_working_directory = os.getcwd()
-inp_wav_dir = current_working_directory + "/" + "data/list"
-
-
-srt_path = "data/subtitle.srt"
-with open(srt_path, 'r') as file:
-      data_dict = json.load(file)
-srt_content = data_dict['srt_content']
-
-
-interence(srt_content=srt_content, audio_list_dir=inp_wav_dir)
+def interence(ref_wav_path, prompt_text, prompt_language, text, text_language, result_path, sovits_path, gpt_path):
+    change_sovits_weights(sovits_path)
+    change_gpt_weights(gpt_path)
+    getTTSWavGenerator = get_tts_wav(ref_wav_path, prompt_text, prompt_language, text, text_language)
+    for simple_rate, audio_data in getTTSWavGenerator:
+        create_wav_file(file_path=result_path, audio_data=audio_data, sampling_rate=simple_rate)
